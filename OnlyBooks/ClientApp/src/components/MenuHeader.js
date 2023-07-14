17b5ebee959d77df4a010d54d740d7b41c18442b
Anis <babaev_anis@mail.ru>
2023-07-15 01:25:44 +0400

    Изменена структура выподающих списков меню, добавлены стили.

--- OnlyBooks/ClientApp/src/components/MenuHeader.js
+++ OnlyBooks/ClientApp/src/components/MenuHeader.js
@@ -2,51 +2,42 @@
 import './NavMenu.css';
 
 const MenuHeader = () => {
-    const [activeDropdown, setActiveDropdown] = useState(null);
-
-    const handleMouseEnter = (dropdownId) => {
-        setActiveDropdown(dropdownId);
-    };
-
-    const handleMouseLeave = () => {
-        setActiveDropdown(null);
-    };
 
     return (
         <div className="menu-header">
             <div className="menu-header-category">
-                <button
-                    className="menu-button"
-                    onMouseEnter={() => handleMouseEnter('books')}
-                    onMouseLeave={handleMouseLeave}
-                >
-                    Книги
-                </button>
-                {activeDropdown === 'books' && (
-                    <div className="category-dropdown">
-                        <ul>
-                            <li>Категория 1</li>
-                            <li>Категория 2</li>
-                            <li>Категория 3</li>
-                        </ul>
-                    </div>
-                )}
-                <button
-                    className="menu-button"
-                    onMouseEnter={() => handleMouseEnter('foreign')}
-                    onMouseLeave={handleMouseLeave}
-                >
-                    Иностранные
-                </button>
-                {activeDropdown === 'foreign' && (
-                    <div className="category-dropdown">
-                        <ul>
-                            <li>Категория 4</li>
-                            <li>Категория 5</li>
-                            <li>Категория 6</li>
-                        </ul>
-                    </div>
-                )}
+				<nav>
+					<ul>
+						<li><a href="#">Книги</a>
+							<ul>
+								<li><a href="#">Главные 2023</a></li>
+								<li><a href="#">Все книги</a></li>
+								<li><a href="#">Билингвы</a></li>
+								<li><a href="#">Комиксы, Манга</a></li>
+								<li><a href="#">Нехудожественная литература</a></li>
+								<li><a href="#">Религия</a></li>
+								<li><a href="#">Художественная литература</a></li>
+								<li><a href="#">Молодежная литература</a></li>
+							</ul>
+						</li>
+
+						<li><a href="#">Иностранные</a>
+							<ul>
+								<li><a href="#">Все книги на иностранном языке</a></li>
+								<li><a href="#">Книги на английском языке</a></li>
+								<li><a href="#">Книги на других языках</a></li>
+								<li><a href="#">Книги на испанском языке</a></li>
+								<li><a href="#">Книги на итальянском языке</a></li>
+								<li><a href="#">Книги на немецком языке</a></li>
+								<li><a href="#">Книги на французском языке</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Главное</a></li>
+						<li><a href="#">Школа</a></li>
+						<li><a href="#">Канцтовары</a></li>
+						<li><a href="#">Приложения</a></li>
+					</ul>
+				</nav>
             </div>
         </div>
     );
